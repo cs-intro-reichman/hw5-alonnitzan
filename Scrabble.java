dc0f4f27e276c761afd5806f5a22db8a32545052
alonnitzan <alon.nitzan@post.runi.ac.il>
2024-12-06 14:40:26 +0200

    Fixed Scrabble

--- Scrabble.java
+++ Scrabble.java
@@ -139,10 +139,8 @@ public class Scrabble {
 		}
 		if (hand.length() == 0) {
 	        System.out.println("Ran out of letters. Total score: " + score + " points");
-			System.out.println();
 		} else {
 			System.out.println("End of hand. Total score: " + score + " points");
-			System.out.println();
 		}
 	}
 
